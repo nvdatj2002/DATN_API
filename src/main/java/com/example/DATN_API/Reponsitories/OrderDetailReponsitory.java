55f8460858a7338c1c4400af64482c1bf1d065ed
nvdatj2002 <92840303+nvdatj2002@users.noreply.github.com>
2023-11-19 15:43:33 +0700

    thong ke

--- src/main/java/com/example/DATN_API/Reponsitories/OrderDetailReponsitory.java
+++ src/main/java/com/example/DATN_API/Reponsitories/OrderDetailReponsitory.java
@@ -2,6 +2,16 @@ package com.example.DATN_API.Reponsitories;
 
 import com.example.DATN_API.Entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderDetailReponsitory extends JpaRepository<OrderDetail, Integer> {
+    @Query("Select o FROM OrderDetail o WHERE o.orders.id = ?1")
+    List<OrderDetail> findByIdOrder(int idOrder);
+
+    @Query("Select o, odt, p FROM OrderCustomer o"
+            + " JOIN o.orderDetails odt JOIN o.status st"
+            + " JOIN odt.productOrder p JOIN p.shop s WHERE s.id = ?1")
+    List<Object[]> getTotalByMonth(int idShop);
 }
--- src/main/java/com/example/DATN_API/Service/OrderDetailService.java
+++ src/main/java/com/example/DATN_API/Service/OrderDetailService.java
@@ -5,11 +5,21 @@ import com.example.DATN_API.Reponsitories.OrderDetailReponsitory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OrderDetailService {
     @Autowired
     OrderDetailReponsitory orderDetailReponsitory;
+
     public OrderDetail save(OrderDetail orderDetail){
         return  orderDetailReponsitory.save(orderDetail);
+    }
+    public List<OrderDetail> findByIdOrder(int idOrder){
+        return orderDetailReponsitory.findByIdOrder(idOrder);
+    }
+
+    public List<Object[]> getTotalByMonth(int idShop){
+        return orderDetailReponsitory.getTotalByMonth(idShop);
     }
 }
